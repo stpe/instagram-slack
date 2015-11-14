38d8c5bf31dd03883be935a8622fa2b998fbbdbf
Stefan Pettersson <stefpet@gmail.com>
2015-11-14 19:01:10 +0100

    Do not reuse slackMsg

--- app/lib/slack.js
+++ app/lib/slack.js
@@ -1,18 +1,18 @@
 var Slack = require("slack-node");
 var _ = require("lodash");
 
-var slackMsg = {
-    channel: "#" + process.env.SLACK_CHANNEL,
-    icon_emoji: process.env.SLACK_ICON_URL,
-    username: process.env.SLACK_USERNAME
-};
-
 var slack = new Slack();
 slack.setWebhook(process.env.SLACK_WEBHOOK);
 
 var slackMessenger = {};
 
 slackMessenger.send = function(msg) {
+    var slackMsg = {
+        channel: "#" + process.env.SLACK_CHANNEL,
+        icon_emoji: process.env.SLACK_ICON_URL,
+        username: process.env.SLACK_USERNAME
+    };
+
     msg = _.merge(slackMsg, msg);
 
     slack.webhook(msg, function(err, response) {
